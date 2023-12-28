de1c0973f321ba8924f5db07a10cd663489207a9
Anton <Anton@DESKTOP-6RE5PBQ>
2023-12-28 03:57:04 +0200

    Добавьте файлы проекта.

---
+++ Lab3.3_Tyagaev/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значение x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество повторений:");
+int n = int.Parse(Console.ReadLine());
+double sum = 0;
+int sign = 1;
+double term = 1;
+for (int i = 0; i < n; i++)
+{
+    sum += sign * term;
+    sign *= -1;
+    term *= x * x / ((2 * i + 2) * (2 * i + 3));
+}
+Console.WriteLine("Сумма: " + sum);
